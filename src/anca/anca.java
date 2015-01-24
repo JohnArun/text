924574d9cb68430b70d08989e0b2bdd6701bd99a
New <New@ArunJohn-PC>
2015-01-25 04:52:14 +1100

    anca class re added

--- src/anca/anca.java
+++ src/anca/anca.java
@@ -4,7 +4,7 @@ public class anca {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Application Software Engineer");
+		System.out.println("anca");
 
 	}
 
